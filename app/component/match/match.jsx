d9853754ae60972a4a9dc50bcd5b993a4314186b
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-03-10 23:26:16 +0100

    Show match date as header

--- app/component/match/match.jsx
+++ app/component/match/match.jsx
@@ -6,9 +6,14 @@ require('./match.css');
 
 module.exports = React.createClass({
     render: function() {
+        var titlePrefix = this.props.match.date || 'Unknown date';
+
         return (
             <li className='match'>
-                <h2 className='match__title'>Match {this.props.match.match_id}</h2>
+                <h2 className='match__title'>
+                    {titlePrefix}
+                    <span className='match__title__id'>{this.props.match.match_id}</span>
+                </h2>
                 <Team players={this.props.match.team1} name='Team 1' winning={this.props.match.winning_team === '1'}/>
                 <Team players={this.props.match.team2} name='Team 2' winning={this.props.match.winning_team === '2'}/>
             </li>
