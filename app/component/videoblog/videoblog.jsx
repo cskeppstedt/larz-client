2907c18bfc2cc74830d4471d5ed42ca1a9659959
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-03-29 21:42:13 +0200

    Move posts state to videblog component

--- app/component/videoblog/videoblog.jsx
+++ app/component/videoblog/videoblog.jsx
@@ -1,14 +1,26 @@
-var React = require('react/addons');
+var Reflux = require('reflux'),
+    React  = require('react/addons'),
+    postsStore = require('../../store/posts');
 
 require('./videoblog.css');
 
 
 module.exports = React.createClass({
+    mixins: [
+        Reflux.connect(postsStore, 'posts')
+    ],
+
+    getInitialState: function() {
+        return {
+            posts: []
+        };
+    },
+
     render: function() {
         return (
             <div className='videoblog'>
                 <h1 className='videoblog__title'>HoNt i veckan</h1>
-                {this.props.posts.map(function(post) {
+                {this.state.posts.map(function(post) {
                     return <iframe
                         className='videoblog__post'
                         src={post.embed_url}
--- app/component/view_controller.jsx
+++ app/component/view_controller.jsx
@@ -1,28 +1,15 @@
-var Reflux = require('reflux'),
-    React  = require('react/addons'),
-    postsStore = require('../store/posts'),
+var React  = require('react/addons'),
     Logo = require('./logo/logo'),
     MatchList = require('./matchlist/matchlist'),
     VideoBlog  = require('./videoblog/videoblog');
 
 
 module.exports = React.createClass({
-    mixins: [
-        Reflux.connect(postsStore, 'posts')
-    ],
-
-    getInitialState: function() {
-        return {
-            posts: []
-        };
-    },
-
-    // <Logo />
-    // <VideoBlog posts={this.state.posts} />
-
     render: function() {
         return (
             <div>
+                <Logo />
+                <VideoBlog />
                 <MatchList />
             </div>
         );
