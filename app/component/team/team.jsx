dc5061e467daaf100eb6708524f94e022879deae
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-04-21 23:30:16 +0200

    WIP stats

--- app/component/team/team.jsx
+++ app/component/team/team.jsx
@@ -37,6 +37,16 @@ module.exports = React.createClass({
                               <span className='player__name'>
                                 {player.nickname}
                               </span>
+                              <dl className='player__stats'>
+                                <dt className='player__stats__label'>LVL</dt>
+                                <dd className='player__stats__value'>{player.level}</dd>
+                                <dt className='player__stats__label'>APM</dt>
+                                <dd className='player__stats__value'>{player.apm}</dd>
+                                <dt className='player__stats__label'>GPM</dt>
+                                <dd className='player__stats__value'>{player.gpm}</dd>
+                                <dt className='player__stats__label'>XPM</dt>
+                                <dd className='player__stats__value'>{player.xpm}</dd>
+                              </dl>
                             </div>
                             <div className='player__wrapper--right'>
                               <span className='player__kills'>{player.herokills}</span>
@@ -50,3 +60,18 @@ module.exports = React.createClass({
         );
     }
 });
+/*
+apm: "122"
+deaths: "5"
+delta_mmr: "4.940"
+denies: "6"
+gpm: "349"
+hero_id: "14"
+heroassists: "10"
+herokills: "8"
+lasthits: "133"
+level: "19"
+nickname: "kha_alim"
+wards: "0"
+xpm: "517"
+*/
