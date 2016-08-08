7a42dff19c94ea6b842864b921c7d744e3a4dfcf
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-08-08 23:41:30 +0200

    Sort players based on name

--- app/store/cs_players.js
+++ app/store/cs_players.js
@@ -1,12 +1,28 @@
 var Reflux = require('reflux'),
     playersUpdate = require('../action/cs_players_update'),
+    playerNames = require('../config/config').players,
+    playerSorter = function (a, b) {
+      var nameA = playerNames[a.steamid].toLowerCase();
+      var nameB = playerNames[b.steamid].toLowerCase();
+
+      if (nameA === nameB) {
+        return 0;
+      } else if (nameA <= nameB) {
+        return -1;
+      } else {
+        return 1;
+      }
+    },
     playersStore = Reflux.createStore({
       init: function() {
         this.listenTo(playersUpdate, this.output);
       },
 
       output: function(players) {
-          this.trigger(players);
+          const sortedPlayers = players.splice(0);
+          sortedPlayers.sort(playerSorter);
+
+          this.trigger(sortedPlayers);
       }
   });
 
