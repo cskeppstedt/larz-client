a54f5dded887350dc2e96fcfbb6321ec97026613
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-08-08 23:00:49 +0200

    Add config keys for CS players

--- app/config/config.js
+++ app/config/config.js
@@ -6,5 +6,15 @@ module.exports = {
         'Schln',
         'skepparn_',
         'zwex'
-    ]
+    ],
+    players: {
+        '76561198311014871': 'Smeknamn Svensson',
+        '76561198041252922': 'skepparn_',
+        '76561198012231365': 'bias',
+        '76561197990372250': 'KÄFTENMORSANJAGHARINTERNET',
+        '76561197969299441': 'Paco',
+        '76561197965118520': 'Tott',
+        '76561197962439381': 'zwex',
+        '76561197960499691': 'khosan'
+    }
 };
