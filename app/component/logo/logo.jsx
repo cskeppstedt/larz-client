d9a07af2ffbed161ae5b25fb525c61144a2275f1
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2019-09-19 20:33:53 +0200

    Add the themesong!

--- app/component/logo/logo.jsx
+++ app/component/logo/logo.jsx
@@ -1,12 +1,23 @@
-var React  = require('react');
+var React = require("react");
 
-require('./logo.css');
+require("./logo.css");
 
+const embedUrl =
+  "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/670122530&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
 
 module.exports = React.createClass({
-    render: function() {
-        return (
-            <div className='logo'/>
-        );
-    }
+  render: function() {
+    return (
+      <div className="themesongWrapper">
+        <iframe
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src={embedUrl}
+          className="themesong"
+        />
+      </div>
+      // <div className='logo'/>
+    );
+  }
 });
